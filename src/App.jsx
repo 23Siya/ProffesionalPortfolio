178f3c143c454f3688ae4e6db1e0b229e91c9b53
23Siya <Siyabongamatsheme@gmail.com>
2025-04-17 11:34:49 +0200

    installing tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-
-import './App.css'
+import './App.css';
+import LoadingScreen from './components/loadingScreen'; // Corrected import (uppercase L)
 
 function App() {
- 
-
   return (
     <>
-     
+      <LoadingScreen /> {/* Corrected component usage (uppercase L) */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
